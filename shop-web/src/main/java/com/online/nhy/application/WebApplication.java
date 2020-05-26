d7aed07038a97d84cf9c7cefb6e221f47c5233cf
ninghaiyang <ninghaiyang@chexiang.com>
2020-05-26 16:30:06 +0800

    22

--- shop-web/src/main/java/com/online/nhy/application/WebApplication.java
+++ shop-web/src/main/java/com/online/nhy/application/WebApplication.java
@@ -1,3 +1,5 @@
+package com.online.nhy.application;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
